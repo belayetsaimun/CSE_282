844dcd5c871a4b8a8457517dfa9930eb1c52aa2c
Belayet Saimun <belayetsaimun16@gmail.com>
2024-05-17 16:09:30 +0600

    Update L8E1.cpp

--- Exp_8/L8E1.cpp
+++ Exp_8/L8E1.cpp
@@ -7,32 +7,28 @@ vector<int> adj[100];
 int dis[100];
 int visited[100];
 
-vector<int> Bfs(int source)
+void Bfs(int source)
 {
-    vector<int> bfs;
     queue<int> q;
     dis[source] = 0;
     visited[source] = 1;
     q.push(source);
-
     while (!q.empty())
     {
         int node = q.front();
-        q.pop();
-        bfs.push_back(node);
         for (auto it : adj[node])
         {
             int nxt_node = it;
+
             if (visited[nxt_node])
                 continue;
             dis[nxt_node] = 1 + dis[node];
             visited[nxt_node] = 1;
             q.push(nxt_node);
         }
+        q.pop();
     }
-    return bfs;
 }
-
 int main()
 {
     int i, j, k;
@@ -55,9 +51,11 @@ int main()
     int source;
     cout << "Enter the source node: " << endl;
     cin >> source;
-    bfs = Bfs(source);
+    Bfs(source);
     for (i = 0; i < n; ++i)
     {
         cout << "Distance " << source << " to " << i << " = " << dis[i] << endl;
     }
 }
+
+   
